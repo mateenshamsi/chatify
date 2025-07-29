cd259b621d34cccefe78767f54998dd3cfd5d087
mateenshamsi <matinshamsi16@gmail.com>
2025-07-29 17:26:01 +0530

    Added logo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,26 @@
 import React, { useEffect } from 'react'
-import { Navigate, Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes, useLocation } from 'react-router-dom'
 import SingupPage from './pages/SingupPage';
 import Login from './pages/LoginPage';
 import { useAuthStore} from './store/useAuthStore';
 import { Toaster } from 'react-hot-toast';
-import HomePage from './pages/HomePage';
-import LoginPage from './pages/LoginPage';
-import ProfilePage from './pages/ProfilePage';
-import SettingsPage from './pages/SettingsPage';
-
+import { HomePage, ProfilePage, SettingsPage,LoginPage,SignupPage } from './pages';
+import Navbar from './components/shared/Navbar';
 
 function App() {
   const {authUser,checkAuth} = useAuthStore()
   useEffect(()=>{
     checkAuth()
   },[checkAuth])
+   const location = useLocation();
+  const noNavbarRoutes = ['/login', '/signup'];
+
+  const showNavbar = !noNavbarRoutes.includes(location.pathname);
   return (
     <div>
-     
+      {showNavbar && <Navbar/>}
       <Routes>
+       
         <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
         <Route path="/signup" element={!authUser ? <SingupPage /> : <Navigate to="/" />} />
         <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
---
+++ frontend/src/components/shared/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+         <img src="/logo.png" alt="Logo" height={150} width={150} />
+    </div>
+  )
+}
+
+export default Navbar
